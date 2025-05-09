4faa1c89fb7f04415453af43a745fcf109388c50
Alejandro Zambrano <alejandro_zambrano_@hotmail.com>
2025-05-09 12:32:53 -0400

    add section Skills

--- src/App.js
+++ src/App.js
@@ -5,26 +5,52 @@ const translations = {
   es: {
     greeting: "¡Hola! Soy Alejandro Zambrano 🇨🇴",
     aboutMeTitle: "Sobre mí",
-    aboutMeText: "Ingeniero industrial y desarrollador web y móvil en Montreal, Canadá. Me destaco por mi actitud colaborativa, mi capacidad para resolver problemas bajo presión y mi disposición a aprender de los errores. Trabajo bien en equipo, acepto la frustración como parte del proceso y siempre busco mejorar.",
+    aboutMeText:
+      "Ingeniero industrial y desarrollador web y móvil en Montreal, Canadá. Me destaco por mi actitud colaborativa, mi capacidad para resolver problemas bajo presión y mi disposición a aprender de los errores. Trabajo bien en equipo, acepto la frustración como parte del proceso y siempre busco mejorar.",
+    skillsTitle: "Competencias",
+    skills: [
+      "Trabajo en equipo",
+      "Resolución de problemas",
+      "Adaptabilidad",
+      "React.js y Node.js",
+      "BAse de datos: MongoDB, MySQL",
+    ],
     projects: "Proyectos Destacados",
     contact: "Contacto",
   },
   en: {
     greeting: "Hi! I'm Alejandro Zambrano 🇨🇦",
     aboutMeTitle: "About Me",
-    aboutMeText: "Industrial engineer and web/mobile developer based in Montreal, Canada. I stand out for my collaborative spirit, ability to solve problems under pressure, and eagerness to learn from mistakes. I work well in teams, accept frustration as part of the process, and always strive to improve.",
+    aboutMeText:
+      "Industrial engineer and web/mobile developer based in Montreal, Canada. I stand out for my collaborative spirit, ability to solve problems under pressure, and eagerness to learn from mistakes. I work well in teams, accept frustration as part of the process, and always strive to improve.",
+    skillsTitle: "Skills",
+    skills: [
+      "Teamwork",
+      "Problem solving",
+      "Adaptability",
+      "React.js and Node.js",
+      "Databases: MongoDB, MySQL",
+    ],
     projects: "Featured Projects",
     contact: "Contact",
   },
   fr: {
     greeting: "Bonjour ! Je suis Alejandro Zambrano 🇨🇦",
     aboutMeTitle: "À propos de moi",
-    aboutMeText: "Ingénieur industriel et développeur web/mobile basé à Montréal, Canada. Je me distingue par mon esprit collaboratif, ma capacité à résoudre des problèmes sous pression et ma volonté d’apprendre de mes erreurs. Je travaille bien en équipe, j’accepte la frustration comme une étape du processus et je cherche toujours à m’améliorer.",
+    aboutMeText:
+      "Ingénieur industriel et développeur web/mobile basé à Montréal, Canada. Je me distingue par mon esprit collaboratif, ma capacité à résoudre des problèmes sous pression et ma volonté d’apprendre de mes erreurs. Je travaille bien en équipe, j’accepte la frustration comme une étape du processus et je cherche toujours à m’améliorer.",
+    skillsTitle: "Compétences",
+    skills: [
+      "Travail en équipe",
+      "Résolution de problèmes",
+      "Adaptabilité",
+      "React.js et Node.js",
+      "Bases de données : MongoDB, MySQL",
+    ],
     projects: "Projets en Vedette",
     contact: "Contact",
   },
 };
-
 
 function App() {
   const [lang, setLang] = useState("es");
@@ -44,40 +70,48 @@ function App() {
         <p>{t.aboutMeText}</p>
       </section>
 
-      <ul className="no-bullets">
-  <li>
-    ✔️ 🚗{" "}
-    <a
-      href="https://github.com/alejandrozambrano/Labo2_CarsApp"
-      target="_blank"
-      rel="noreferrer"
-    >
-      Labo2_CarsApp – App Android con SQLite y cámara
-    </a>
-  </li>
-  <li>
-    ✔️ 🧱{" "}
-    <a
-      href="https://github.com/alejandrozambrano/Fundamentos-de-HTML"
-      target="_blank"
-      rel="noreferrer"
-    >
-      Fundamentos de HTML – Proyecto final
-    </a>
-  </li>
-  <li>
-    ✔️ 🎥{" "}
-    <a
-      href="https://github.com/alejandrozambrano/projectClient"
-      target="_blank"
-      rel="noreferrer"
-    >
-      Streaming Site – React, Node.js y MongoDB
-    </a>
-  </li>
-</ul>
-   
+      <section className="skills">
+       <h2>{t.skillsTitle}</h2>
+        <ul className="no-bullets">
+         {t.skills.map((skill, index) => (
+          <li key={index}>✅ {skill}</li>
+         ))}
+        </ul>
+        </section>
 
+
+      <ul className="no-bullets">
+        <li>
+          ✔️ 🚗{" "}
+          <a
+            href="https://github.com/alejandrozambrano/Labo2_CarsApp"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Labo2_CarsApp – App Android con SQLite y cámara
+          </a>
+        </li>
+        <li>
+          ✔️ 🧱{" "}
+          <a
+            href="https://github.com/alejandrozambrano/Fundamentos-de-HTML"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Fundamentos de HTML – Proyecto final
+          </a>
+        </li>
+        <li>
+          ✔️ 🎥{" "}
+          <a
+            href="https://github.com/alejandrozambrano/projectClient"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Streaming Site – React, Node.js y MongoDB
+          </a>
+        </li>
+      </ul>
 
       <h2>{t.contact}</h2>
       <p>
